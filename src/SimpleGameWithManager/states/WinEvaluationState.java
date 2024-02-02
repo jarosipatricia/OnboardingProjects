212a2a3387749e3190d25f0e47a3f12c5a7beff6
pjaro <pjarosiova@greentube.com>
2024-02-02 14:46:01 +0100

    Win animation

--- src/SimpleGameWithManager/states/WinEvaluationState.java
+++ src/SimpleGameWithManager/states/WinEvaluationState.java
@@ -20,10 +20,28 @@ public class WinEvaluationState extends AAnimationState implements IGameState, I
     @Override
     public void enter() {
         System.out.println("Entering Win Evaluation State");
+
+        int winAmount = new Random().nextInt(101); //random number between 0 and 100
+
+        if (winAmount > 50) {
+            playBigWinAnimation();
+        }
     }
     @Override
     public void exit(IOnStateFinished onStateFinishedCallback) {
         StateManager.getManager().changeState(new WinAnimationState(new WinAnimation()));
         onStateFinishedCallback.onFinished();
+    }
+    private void playBigWinAnimation() {
+        System.out.println("\u001B[33m$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\u001B[0m");
+
+        System.out.println("\u001B[33m  ____    _____    _____    __          __  _____   _   _ \u001B[0m");
+        System.out.println("\u001B[33m |  _ \\  |_   _|  / ____|   \\ \\        / / |_   _| | \\ | |\u001B[0m");
+        System.out.println("\u001B[33m | |_) |   | |   | |  __     \\ \\  /\\  / /    | |   |  \\| |\u001B[0m");
+        System.out.println("\u001B[33m |  _ <    | |   | | |_ |     \\ \\/  \\/ /     | |   | . ` |\u001B[0m");
+        System.out.println("\u001B[33m | |_) |  _| |_  | |__| |      \\  /\\  /     _| |_  | |\\  |\u001B[0m");
+        System.out.println("\u001B[33m |____/  |_____|  \\_____|       \\/  \\/     |_____| |_| \\_|\u001B[0m");
+
+        System.out.println("\u001B[33m$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\u001B[0m");
     }
 }
